bdb2d649be8af3826cb1a6de796e42109fdf0906
Mateus Vasconcellos <mateusdepaula3@gmail.com>
2022-11-08 11:48:21 -0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className="mx-auto grid h-screen max-w-[1124px] grid-cols-2 items-center gap-28">
+      Hello word
+    </div>
+  );
+}
